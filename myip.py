778bff6c24a57c478a7e1f518ec17c682eedd8bb
Alias Lyend <Lyend.chatango@gmail.com>
2016-09-18 16:57:13 -0500

    Update myip.py

--- myip.py
+++ myip.py
@@ -1,2 +1,6 @@
 from urllib import urlopen
+from socket import socket, SOCK_DGRAM, AF_INET
+s = socket(AF_INET, SOCK_DGRAM)
+s.connect(('google.com', 0))
+print('Local  IP: '+s.getsockname()[0])
 print('Public IP: '+urlopen('http://ipv4bot.whatismyipaddress.com/').read().replace('\n', ''))
